966034d065eb47c671f6106f70a49330dfa2a313
Joan Moreau <jom@grosjo.net>
2019-01-13 21:52:49 +0000

    Missing files

--- fts-xapian-config.h
+++
@@ -1,6 +0,0 @@
-/* fts-xapian-config.h.  Generated from fts-xapian-config.h.in by configure.  */
-/* Define to the full name of Dovecot FTS Xapian. */
-#define FTS_XAPIAN_NAME "Dovecot FTS Xapian"
-
-/* Define to the version of Dovecot FTS Xapian. */
-#define FTS_XAPIAN_VERSION "1.0b2"
