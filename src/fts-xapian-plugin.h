d5012e32302e16d2578dea3681a13ddddd965b15
André Rodier <andre@rodier.me>
2021-03-26 20:13:21 +0000

    Continued to reformat source using astyle
    See https://manpages.ubuntu.com/manpages/trusty/man1/astyle.1.html
    Related to issue #76

--- src/fts-xapian-plugin.h
+++ src/fts-xapian-plugin.h
@@ -1,17 +1,17 @@
-/* Copyright (c) 2019 Joan Moreau <jom@grosjo.net>, see the included COPYING file */
-
+/* Copyright (c) 2019 Joan Moreau <jom@grosjo.net>, see the included COPYING
+ * file */
 
 #ifndef FTS_XAPIAN_PLUGIN_H
 #define FTS_XAPIAN_PLUGIN_H
 
-#include "lib.h"
-#include "mail-storage-private.h"
-#include "mailbox-list-private.h"
-#include "mail-search.h"
-#include "fts-api.h"
-#include "module-context.h"
-#include "mail-user.h"
 #include "fts-api-private.h"
+#include "fts-api.h"
+#include "lib.h"
+#include "mail-search.h"
+#include "mail-storage-private.h"
+#include "mail-user.h"
+#include "mailbox-list-private.h"
+#include "module-context.h"
 #include "restrict-process-size.h"
 
 extern const char *fts_xapian_plugin_dependencies[];
