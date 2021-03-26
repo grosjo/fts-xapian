399549facfe5d435de7ad0b9cab8348ae969bf67
André Rodier <andre@rodier.me>
2021-03-26 20:12:34 +0000

    Continued to reformat source using astyle
    See https://manpages.ubuntu.com/manpages/trusty/man1/astyle.1.html
    Related to issue #76

--- src/fts-xapian-plugin.c
+++ src/fts-xapian-plugin.c
@@ -1,18 +1,16 @@
-/* Copyright (c) 2019 Joan Moreau <jom@grosjo.net>, see the included COPYING file */
-
+/* Copyright (c) 2019 Joan Moreau <jom@grosjo.net>, see the included COPYING
+ * file */
 
 #include "fts-xapian-plugin.h"
 
 const char *fts_xapian_plugin_version = DOVECOT_ABI_VERSION;
 
-void fts_xapian_plugin_init(struct module *module ATTR_UNUSED)
-{
-	fts_backend_register(&fts_backend_xapian);
+void fts_xapian_plugin_init(struct module *module ATTR_UNUSED) {
+  fts_backend_register(&fts_backend_xapian);
 }
 
-void fts_xapian_plugin_deinit(void)
-{
-	fts_backend_unregister(fts_backend_xapian.name);
+void fts_xapian_plugin_deinit(void) {
+  fts_backend_unregister(fts_backend_xapian.name);
 }
 
-const char *fts_xapian_plugin_dependencies[] = { "fts", NULL };
+const char *fts_xapian_plugin_dependencies[] = {"fts", NULL};
