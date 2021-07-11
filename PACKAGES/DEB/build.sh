f75cb1aa7003dd42003084957655ae2c1054ba00
Joan <jom@grosjo.net>
2021-07-11 22:17:44 +0300

    Issue #87

--- PACKAGES/DEB/build.sh
+++ PACKAGES/DEB/build.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 #HELP : https://saveriomiroddi.github.io/Building-a-debian-deb-source-package-and-publishing-it-on-an-ubuntu-ppa/#the-procedure
-export VERSION=1.4.11
-rm dovecot*
-git clone https://github.com/grosjo/fts-xapian.git
-mv fts-xapian dovecot-fts-xapian-${VERSION}
+export VERSION=1.4.11-3
+export BASE=1.4.11
+rm -rf dovecot*
+git clone https://github.com/grosjo/fts-xapian.git dovecot-fts-xapian-${VERSION}
+tar -czf dovecot-fts-xapian-${VERSION}_${BASE}.orig.tar.gz dovecot-fts-xapian-${VERSION}
 cd dovecot-fts-xapian-${VERSION}
 autoreconf -vi
 ./configure --with-dovecot=/usr/lib/dovecot
 make
 dh_make -p dovecot-fts-xapian-${VERSION} --single --native --copyright gpl2 --email jom@grosjo.net -y
 rm debian/*.ex debian/*.EX
-perl -i -pe 's/^(Section:).*/$1 utils/' debian/control
-perl -i -pe 's/^(Homepage:).*/$1 https:\/\/testpackage.barryfoo.org/'              debian/control
-perl -i -pe 's/^#(Vcs-Browser:).*/$1 https:\/\/github.com\/barryfoo\/testpackage/' debian/control
-perl -i -pe 's/^#(Vcs-Git:).*/$1 https:\/\/github.com\/barryfoo\/testpackage.git/' debian/control
-perl -i -pe 's/^(Standards-Version:) 3.9.6/$1 3.9.7/' debian/control
 cp ../control debian/
 cp ../changelog debian/
 debuild -S | tee /tmp/debuild.log 2>&1
