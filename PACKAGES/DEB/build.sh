bd43d16e6be971dfb21918bb6d429c4d3a6315dd
Joan <jom@grosjo.net>
2021-07-11 23:05:28 +0300

    Issue #87

--- PACKAGES/DEB/build.sh
+++ PACKAGES/DEB/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #HELP : https://saveriomiroddi.github.io/Building-a-debian-deb-source-package-and-publishing-it-on-an-ubuntu-ppa/#the-procedure
-export VERSION=1.4.11-4
+export VERSION=1.4.11-6
 export BASE=1.4.11
 rm -rf dovecot*
 git clone https://github.com/grosjo/fts-xapian.git dovecot-fts-xapian-${VERSION}
@@ -13,6 +13,7 @@ dh_make -p dovecot-fts-xapian-${VERSION} --single --native --copyright gpl2 --em
 rm debian/*.ex debian/*.EX
 cp ../control debian/
 cp ../changelog debian/
+cp ../compat debian/
 debuild -S | tee /tmp/debuild.log 2>&1
 cd ..
 dput ppa:grosjo/dovecot-fts-xapian dovecot-fts-xapian-${VERSION}_${VERSION}_source.changes
