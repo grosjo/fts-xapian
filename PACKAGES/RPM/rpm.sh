b0205c07ea65cec7dd88b9bfb14e3a91c1863c99
Joan Moreau <jom@grosjo.net>
2021-08-29 11:23:56 +0100

    Issue #82

--- PACKAGES/RPM/rpm.sh
+++ PACKAGES/RPM/rpm.sh
@@ -4,8 +4,10 @@ rpmdev-setuptree
 cat dovecot-fts-xapian.spec.in | sed -e s/FTSVERSION/${VERSION}/g > dovecot-fts-xapian.spec
 cp dovecot-fts-xapian.spec ~/rpmbuild/SPECS/
 wget https://github.com/grosjo/fts-xapian/archive/refs/tags/${VERSION}.tar.gz -O ~/rpmbuild/SOURCES/dovecot-fts-xapian-${VERSION}.tar.gz
-QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
-QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
+# QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
+rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
+# QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
+rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
 cp ~/rpmbuild/SRPMS/dovecot-fts-xapian-${VERSION}-1.fc34.src.rpm ${REP}/
 cp ~/rpmbuild/RPMS/x86_64/dovecot-fts-xapian-${VERSION}-1.fc34.x86_64.rpm ${REP}/
 koji build --scratch f34 ./dovecot-fts-xapian-${VERSION}-1.fc34.src.rpm
