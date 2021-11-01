0caad1cc46f6a8881fef0933fc7149ec7d835f23
Joan Moreau <jom@grosjo.net>
2021-11-01 12:58:28 +0000

    Issue #99

--- src/fts-xapian-plugin.c
+++ src/fts-xapian-plugin.c
@@ -5,14 +5,101 @@
 
 const char *fts_xapian_plugin_version = DOVECOT_ABI_VERSION;
 
+struct fts_xapian_user_module fts_xapian_user_module = MODULE_CONTEXT_INIT(&mail_user_module_register);
+
+
+
+static void fts_xapian_mail_user_deinit(struct mail_user *user)
+{
+	struct fts_xapian_user *fuser = FTS_XAPIAN_USER_CONTEXT_REQUIRE(user);
+
+        fts_mail_user_deinit(user);
+	fuser->module_ctx.super.deinit(user);
+}
+
 static void fts_xapian_mail_user_created(struct mail_user *user)
 {
-        struct mail_user_vfuncs *v = user->vlast;
-        const char *env, *error;
+        const char *error;
 
-        if (fts_mail_user_init(user, FALSE, &error) < 0) {
-                i_error("FTS Xapian: %s", error);
+	struct mail_user_vfuncs *v = user->vlast;
+	struct fts_xapian_user *fuser;
+
+	fuser = p_new(user->pool, struct fts_xapian_user, 1);
+
+        fuser->set.verbose 	= 0;
+        fuser->set.lowmemory 	= XAPIAN_MIN_RAM;
+        fuser->set.partial 	= XAPIAN_DEFAULT_PARTIAL;
+        fuser->set.full 	= XAPIAN_DEFAULT_FULL;
+	
+	const char * env = mail_user_plugin_getenv(user, "fts_xapian");
+        if (env == NULL)
+        {
+                i_warning("FTS Xapian: missing configuration - Using default values");
         }
+
+	long len;
+	const char *const *tmp;
+
+        for (tmp = t_strsplit_spaces(env, " "); *tmp != NULL; tmp++)
+        {
+                if (strncmp(*tmp, "partial=",8)==0)
+                {
+                        len=atol(*tmp + 8);
+                        if(len<2)
+                        {
+                                i_error("FTS Xapian: 'partial' parameter is incorrect (%ld). Try 'partial=%ld'",len,XAPIAN_DEFAULT_PARTIAL);
+                                len=XAPIAN_DEFAULT_PARTIAL;
+                        }
+                        fuser->set.partial = len;
+                }
+                else if (strncmp(*tmp,"full=",5)==0)
+                {
+                        len=atol(*tmp + 5);
+                        if(len<1)
+                        {
+                                i_error("FTS Xapian: 'full' parameter is incorrect (%ld). Try 'full=%ld'",len,XAPIAN_DEFAULT_FULL);
+                        } 
+			else if(len>40)
+                        {
+                                i_error("FTS Xapian: 'full' parameter above 50 (%ld) is not realistic",len);
+                        }
+                        else fuser->set.full = len;
+                }
+                else if (strncmp(*tmp,"verbose=",8)==0)
+                {
+                        len=atol(*tmp + 8);
+                        if(len>0) { fuser->set.verbose = len; }
+                }
+                else if (strncmp(*tmp,"lowmemory=",10)==0)
+                {
+                        len=atol(*tmp + 9);
+                        if(len>0) { fuser->set.lowmemory = len; }
+                }
+                else if (strncmp(*tmp,"attachments=",12)==0)
+                {
+                        // Legacy
+                }
+                else
+                {
+                        i_error("FTS Xapian: Invalid setting: %s", *tmp);
+                }
+        }
+
+        if(fuser->set.full < fuser->set.partial)
+        {
+                i_error("FTS Xapian: 'full' (%ld) parameter must be equal or greater than 'partial' (%ld)",fuser->set.full,fuser->set.partial);
+                fuser->set.partial = XAPIAN_DEFAULT_PARTIAL;
+                fuser->set.full = XAPIAN_DEFAULT_FULL;
+        }
+
+
+	if (fts_mail_user_init(user, FALSE, &error) < 0) i_error("FTS Xapian: %s", error);
+
+	fuser->module_ctx.super = *v;
+	user->vlast = &fuser->module_ctx.super;
+	v->deinit = fts_xapian_mail_user_deinit;
+
+	MODULE_CONTEXT_SET(user, fts_xapian_user_module, fuser);
 }
 
 static struct mail_storage_hooks fts_xapian_mail_storage_hooks = 
--- src/fts-xapian-plugin.h
+++ src/fts-xapian-plugin.h
@@ -5,17 +5,43 @@
 #define FTS_XAPIAN_PLUGIN_H
 
 #include "lib.h"
-#include "fts-api-private.h"
 #include "fts-api.h"
 #include "fts-user.h"
 #include "mail-search.h"
 #include "mail-storage-private.h"
-#include "mail-user.h"
-#include "module-context.h"
 #include "restrict-process-size.h"
 #include "mail-storage-hooks.h"
+#include "module-context.h"
+#include "mail-user.h"
+#include "fts-api-private.h"
+
+#define XAPIAN_FILE_PREFIX "xapian-indexes"
+#define XAPIAN_TERM_SIZELIMIT 245L
+#define XAPIAN_COMMIT_ENTRIES 1000000L
+#define XAPIAN_COMMIT_TIMEOUT 300L
+#define XAPIAN_WILDCARD "wldcrd"
+#define XAPIAN_EXPUNGE_HEADER 9
+#define XAPIAN_MIN_RAM 200L // MB
+#define XAPIAN_DEFAULT_PARTIAL 3L
+#define XAPIAN_DEFAULT_FULL 20L
+
+struct fts_xapian_settings
+{
+        int verbose;
+        long lowmemory;
+        long partial,full;
+};
+
+struct fts_xapian_user {
+        union mail_user_module_context module_ctx;
+        struct fts_xapian_settings set;
+};
+
+#define FTS_XAPIAN_USER_CONTEXT(obj) (struct fts_xapian_user *)MODULE_CONTEXT(obj, fts_xapian_user_module)
+#define FTS_XAPIAN_USER_CONTEXT_REQUIRE(obj) MODULE_CONTEXT_REQUIRE(obj, fts_xapian_user_module)
 
 extern const char *fts_xapian_plugin_dependencies[];
+extern MODULE_CONTEXT_DEFINE(fts_xapian_user_module, &mail_user_module_register);
 extern struct fts_backend fts_backend_xapian;
 
 void fts_xapian_plugin_init(struct module *module);
