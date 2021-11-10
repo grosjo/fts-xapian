0b5cd5ff579bd225339260c882f56a6c40ac1e9a
Joan Moreau <jom@grosjo.net>
2021-11-10 21:05:40 +0000

    Fedora 1.5.0

--- PACKAGES/RPM/rpm.sh
+++ PACKAGES/RPM/rpm.sh
@@ -1,14 +1,12 @@
 VERSION=1.5.0
+FEDORA=fc35
 REP=`pwd`
 rpmdev-setuptree
 cat dovecot-fts-xapian.spec.in | sed -e s/FTSVERSION/${VERSION}/g > dovecot-fts-xapian.spec
 cp dovecot-fts-xapian.spec ~/rpmbuild/SPECS/
 wget https://github.com/grosjo/fts-xapian/archive/refs/tags/${VERSION}.tar.gz -O ~/rpmbuild/SOURCES/dovecot-fts-xapian-${VERSION}.tar.gz
-# QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
-rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
-# QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
-rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
-cp ~/rpmbuild/SRPMS/dovecot-fts-xapian-${VERSION}-1.fc34.src.rpm ${REP}/
-cp ~/rpmbuild/RPMS/x86_64/dovecot-fts-xapian-${VERSION}-1.fc34.x86_64.rpm ${REP}/
-koji build --scratch f34 ./dovecot-fts-xapian-${VERSION}-1.fc34.src.rpm
+QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bb --nodebuginfo ~/rpmbuild/SPECS/dovecot-fts-xapian.spec
+cp ~/rpmbuild/SRPMS/dovecot-fts-xapian-${VERSION}-1.${FEDORA}.src.rpm ${REP}/
+cp ~/rpmbuild/RPMS/x86_64/dovecot-fts-xapian-${VERSION}-1.${FEDORA}.x86_64.rpm ${REP}/
+koji build --scratch f35 ./dovecot-fts-xapian-${VERSION}-1.${FEDORA}.src.rpm
 
