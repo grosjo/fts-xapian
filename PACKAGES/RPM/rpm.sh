cc5d8a084323949f56220b640e785254ac7aa291
Joan Moreau <jom@grosjo.net>
2021-11-12 13:52:34 +0000

    Version 1.5.1

--- PACKAGES/RPM/rpm.sh
+++ PACKAGES/RPM/rpm.sh
@@ -1,6 +1,7 @@
 VERSION=1.5.1
 FEDORA=fc35
 REP=`pwd`
+kinit grosjo@FEDORAPROJECT.ORG
 rpmdev-setuptree
 cat dovecot-fts-xapian.spec.in | sed -e s/FTSVERSION/${VERSION}/g > dovecot-fts-xapian.spec
 cp dovecot-fts-xapian.spec ~/rpmbuild/SPECS/
@@ -10,4 +11,24 @@ QA_RPATHS=$(( 0x0001|0x0010 )) rpmbuild --bs --nodebuginfo ~/rpmbuild/SPECS/dove
 cp ~/rpmbuild/SRPMS/dovecot-fts-xapian-${VERSION}-1.${FEDORA}.src.rpm ${REP}/
 cp ~/rpmbuild/RPMS/x86_64/dovecot-fts-xapian-${VERSION}-1.${FEDORA}.x86_64.rpm ${REP}/
 koji build --scratch f35 ./dovecot-fts-xapian-${VERSION}-1.${FEDORA}.src.rpm
+rm -rf fedora
+mkdir fedora
+cd fedora
+fedpkg clone dovecot-fts-xapian
+fedpkg switch-branch rawhide
+cp ../../dovecot-fts-xapian.spec ./
+git add dovecot-fts-xapian.spec
+git commit -m "Version $VERSION}"
+git push
+fedpkg import ../../dovecot-fts-xapian-${VERSION}-1.${FEDORA}.src.rpm
+fedpkg build
+fedpkg switch-branch f35
+git merge rawhide
+fedpkg build
+fedpkg switch-branch f34
+git merge rawhide
+fedpkg build
+fedpkg switch-branch el8
+git merge rawhide
+fedpkg build
 
