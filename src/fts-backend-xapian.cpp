f06f020e6380c8fd22235746f122226180d204a1
Joan Moreau <jom@grosjo.net>
2021-11-22 18:57:54 +0000

    Issue #107

--- src/fts-backend-xapian.cpp
+++ src/fts-backend-xapian.cpp
@@ -414,7 +414,7 @@ static int fts_backend_xapian_update_build_more(struct fts_backend_update_contex
 		{
 			char * t = i_strdup("NODATA");
 			if(data != NULL) { i_free(t); t = i_strndup(data,40); }
-			i_info("FTS Xapian: Indexing part as attachment (data like '%s')",t);
+			i_info("FTS Xapian: Indexing part as attachment");
 			i_free(t);
 		}
 		else
